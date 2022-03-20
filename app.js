cc3f808b9e9fd7a58c8858e75d47b0a22314ca31
Ahmed Elgreetly <aelgreetly@yahoo.com>
2022-03-20 20:51:42 +0300

    db and access logic update

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const express = require("express");
+const db = require("./utils/db");
+const { resolveToken } = require("./utils/tokenMiddleware");
+
+db.set();
+
+const users = require("./controllers/users");
+const posts = require("./controllers/posts");
 
 const app = express();
+const { Router } = express;
+
+app.use(express.json());
+
+app.use("/users", users(new Router()));
+app.use("/posts", resolveToken, posts(new Router()));
 
 module.exports = app;
--- controllers/users.js
+++ controllers/users.js
@@ -1,18 +1,32 @@
+const db = require("../utils/db");
+const { resolveToken } = require("../utils/tokenMiddleware");
+
 module.exports = (app) => {
-  const users = [];
-
   app.post("/register", (req, res) => {
-    /**
-     * name
-     * email
-     * password
-     */
     const { body } = req;
-    users.push(body);
-
+    const data = db.getData();
+    if (body.username && body.password && body.name) {
+      data.users.push(body);
+      db.saveData(data);
+    }
     res.json(body);
   });
-  app.post("/login", (req, res) => {});
-  app.get("/me", (req, res) => {});
+
+  app.post("/login", (req, res) => {
+    const { username, password } = req.body;
+
+    const { users } = db.getData();
+    const user = users.find(
+      (u) => u.username === username && u.password === password
+    );
+
+    if (!user) return res.status(403).end();
+    user.token = users.findIndex((u) => u.username === username);
+    res.json(user);
+  });
+
+  app.get("/me", resolveToken, (req, res) => {
+    res.json(req.user);
+  });
   return app;
 };
